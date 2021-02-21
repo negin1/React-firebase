51d166945d9c9cc703c1bb579c29f9a36886f678
negin1 <negin.jalalian@gmail.com>
2021-02-21 18:36:20 +0100

    Database Users with Roles

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database';
 
 
 const config = {
@@ -18,9 +19,14 @@ class Firebase {
     app.initializeApp(config);
 
     this.auth = app.auth();
+    this.db = app.database();
   }
 
   // *** Auth API ***
+
+  user = uid => this.db.ref(`users/${uid}`);
+  
+  users = () => this.db.ref('users');
 
   doCreateUserWithEmailAndPassword = (email, password) =>
     this.auth.createUserWithEmailAndPassword(email, password);
